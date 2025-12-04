81b84ace18c373ca35ffd156542315cad695e468
LearnWithMazbaul <learnwithmazbaul@gmail.com>
2025-12-04 14:14:52 +0600

    05.Custom page setup and login logout in breeze package

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\DashboardController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -16,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //logout route
+    Route::get('/logout', [DashboardController::class, 'logout'])->name('logout');
 });
 
 require __DIR__.'/auth.php';
