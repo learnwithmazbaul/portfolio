feaa99750ff2a0c8d1e3e892dbd65a0be30fe186
LearnWithMazbaul <learnwithmazbaul@gmail.com>
2025-12-04 13:52:51 +0600

    04.Backend blade templating

---
+++ portfolio/resources/views/backend/layout/app.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="{{ asset('backend/css/styles.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <style>
+        .active {
+            color: white;
+        }
+
+        .fl-wrapper {
+            z-index: 99999 !important;
+        }
+    </style>
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    @include('backend.layout.topNav')
+
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            @include('backend.layout.sideNav')
+        </div>
+        <div id="layoutSidenav_content">
+            <!-- main content start -->
+            <main>
+                @yield('content')
+            </main>
+            <!-- main content end -->
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2025</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script src="{{ asset('backend/js/scripts.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('backend/assets/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('backend/assets/demo/chart-bar-demo.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
+    <script src="{{ asset('backend/js/datatables-simple-demo.js') }}"></script>
+
+    <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#photo').change(function (e) {
+                var file = e.target.files[0];
+
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    $('#showImage').attr('src', event.target.result);
+                }
+                reader.readAsDataURL(file);
+            });
+        });
+    </script>
+</body>
+
+</html>
--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ProfileController;
 // });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -24,3 +24,4 @@ Route::get('/', [PageController::class, 'index'])->name('index');
 Route::get('/resume', [PageController::class, 'resume'])->name('resume');
 Route::get('/projects', [PageController::class, 'projects'])->name('projects');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
