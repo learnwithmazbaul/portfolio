57f304ed6d98e1aad1706c7e0eef47635da6829b
LearnWithMazbaul <learnwithmazbaul@gmail.com>
2026-01-24 18:52:13 +0600

    19.Resume Page Backend Implementation

--- portfolio/app/Http/Controllers/PageController.php
+++ portfolio/app/Http/Controllers/PageController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\Skill;
+use App\Models\Resume;
 use App\Models\Social;
+use App\Models\Language;
+use App\Models\Education;
+use App\Models\Experience;
 use App\Models\HeroProperty;
 use Illuminate\Http\Request;
 
@@ -17,7 +22,12 @@ class PageController extends Controller
     }
 
     public function resume(){
-        return view('frontend.pages.resume');
+        $resume = Resume::first();
+        $experiences = Experience::all();
+        $educations = Education::all();
+        $skills = Skill::all();
+        $languages = Language::all();
+        return view('frontend.pages.resume',compact('resume','experiences','educations','skills','languages'));
     }
 
     public function projects(){
